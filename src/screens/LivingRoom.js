90eeb866528578d2a07b502aa04947ffbb2d7069
Manu puranic <68289159+manupuranic@users.noreply.github.com>
2021-07-17 12:52:27 +0530

    ip address trial 1

--- src/screens/LivingRoom.js
+++ src/screens/LivingRoom.js
@@ -1,16 +1,39 @@
-import React, { useState } from "react";
-import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
+import React from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Linking,
+} from "react-native";
 
 const LivingRoom = () => {
-  const [lightsOn, setLightsOn] = useState(false);
+  const fetchLightsOff = async () => {
+    Linking.openURL("http://192.168.161/16/off");
+  };
+
+  const fetchLightsOn = async () => {
+    Linking.openURL("http://192.168.161/16/on");
+    // const response = await fetch("http://192.168.161/16/on");
+    // if (!response.ok) {
+    //   throw new Error("Something went wrong");
+    // }
+    // const data = await response.json();
+    // console.log(data);
+  };
 
   return (
     <View>
       <Text style={styles.heading}>Living Room</Text>
       <View style={styles.lightContainer}>
-        <TouchableOpacity onPress={() => setLightsOn(!lightsOn)}>
+        <TouchableOpacity onPress={fetchLightsOn}>
           <View style={styles.lightButton}>
-            <Text style={styles.lightText}>Turn {lightsOn ? "Off" : "On"}</Text>
+            <Text style={styles.lightText}>Turn On</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={fetchLightsOff}>
+          <View style={styles.lightButton}>
+            <Text style={styles.lightText}>Turn Off</Text>
           </View>
         </TouchableOpacity>
       </View>
@@ -32,6 +55,7 @@ const styles = StyleSheet.create({
   lightButton: {
     borderColor: "black",
     borderWidth: 1,
+    marginBottom: 20,
     padding: 30,
     width: 200,
   },
